4f5bcb0d31ec1cd9b1297a2f6aeb4494e9a0345a
Afnan011 <muhammedafnan8184@gmail.com>
2023-12-09 10:48:09 +0530

    boiler code removed. files organized

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('parcel guru'),
+        ),
+        body:
+        const Center(
+          child: Text("Parcel Guru"),
+        )
+    );
+  }
+}
