2bb71c48f25f81e79cb084c3d1b14bc2afaae8de
mubasshir <mahammad.mubassir@gmail.com>
2023-12-09 15:19:28 +0530

    Custom widget button

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:parcel_guru/screens/home_page.dart';
-import 'package:parcel_guru/screens/signup_pages/signup.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +19,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         fontFamily: 'Inter'
       ),
-      home: const MySignUpPage(),
+      home: const MyHomePage(),
     );
   }
 }
